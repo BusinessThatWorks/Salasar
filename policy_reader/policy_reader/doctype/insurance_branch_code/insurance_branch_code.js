ae87799755877397fa10909c9d81160b0771b4ee
Jai V. Kejriwal <jai.kejriwal@gmail.com>
2025-10-15 16:26:30 +0530

    added insurance doctypes of branch code, csc_code, customer, customer_group and employee

---
+++ policy_reader/policy_reader/doctype/insurance_branch_code/insurance_branch_code.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Clapgrow Software and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Insurance Branch Code", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ policy_reader/policy_reader/doctype/insurance_csc_code/insurance_csc_code.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Clapgrow Software and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class InsuranceCSCCode(Document):
+	pass
